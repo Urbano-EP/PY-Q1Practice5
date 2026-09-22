57e07f8176102e0025a2c87894ad7c4c6a9eb933
Urbano-EP <urbano.erp@obmontessori.edu.ph>
2026-09-22 13:01:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ def display_char(e):
     get_product = document.getElementById("product").value
     get_stock = document.getElementById("stock").value
 
-    display(get_category[:3] + get_product[:3] + get_stock[:3], target="output1") #get the 1st 3 letters of the word
+    SKU = get_category[:3].upper() + "product" + get_product[:4].upper() + "stock" + str(get_stock)
+
+    display("SKU: ", SKU, target='output1')
