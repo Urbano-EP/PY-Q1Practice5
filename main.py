2332cf2114d5e8ef2f74a74bdf827f5a47af89f2
Urbano-EP <urbano.erp@obmontessori.edu.ph>
2026-09-22 12:55:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pyscript import display, document
 def display_char(e):
     document.getElementById("output1").innerHTML = ""
 
-    get_word = document.getElementById("word").value
+    get_category = document.getElementById("category").value
+    get_product = document.getElementById("product").value
+    get_stock = document.getElementById("stock").value
 
-    display(get_word[:3], target="output1") #get the 1st 3 letters of the word
+    display(get_category[:3] + get_product[:3] + get_stock[:3], target="output1") #get the 1st 3 letters of the word
