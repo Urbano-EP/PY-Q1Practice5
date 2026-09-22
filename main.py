ac04cc2191236967c6da396086078e0ad4d82944
Urbano-EP <urbano.erp@obmontessori.edu.ph>
2026-09-22 13:18:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def display_char(e):
     get_product = document.getElementById("product").value
     get_stock = document.getElementById("stock").value
 
-    SKU = get_category[:3].upper() + "product" + get_product[:4].upper() + "stock" + str(get_stock)
+    SKU = get_category[:3].upper() + "-" + get_product[:4].upper() + "-" + str(get_stock)
 
     display("SKU: ", SKU, target='output1')
